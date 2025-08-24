b28fb19ba917464caa81930928d37dc9f38b1ba8
tontnm <tontnm@gmail.com>
2025-08-24 22:00:12 +0700

    postgrest db design many to many

--- src/sql/apostgresql/11-like-system/11-like-system.sql
+++ src/sql/apostgresql/11-like-system/11-like-system.sql
@@ -61,7 +61,38 @@ ADD CHECK OF
 SELECT COALESCE((NULL)::BOOLEAN::INTEGER,0); --0
 SELECT NULL::BOOLEAN; --NULL
 SELECT NULL::BOOLEAN::INTEGER; --NULL
-SELECT 1::BOOLEAN; --true
-SELECT 1::BOOLEAN::INTEGER; --1
+SELECT 534::BOOLEAN; --true
+SELECT 555::BOOLEAN::INTEGER; --1
 SELECT COALESCE(1,0); --1
 SELECT COALESCE((1)::BOOLEAN::INTEGER,0); --1
+
+/*
+cách 1: polymorphic association
+- vấn đề nằm ở cột liked_id, nó ko thể làm foreign key cho cả post và comment
+- cho nên có thể sẽ để liked_id là 9999 mà ko bị postgresql phát hiện lỗi
+
+cách 2: alternative
+- sử dụng 2 cột liked_post_id, liked_comment_id để phân biệt, đây là 2 foreign key thực thụ
+- khi like 1 cái thì chỉ cần điền vào 1 trong 2 cột
+- nếu không có cái nào thì sẽ là NULL
+- có 1 vấn đề đó là post_id và comment_id cùng tồn tại hoặc cả 2 đều NULL, 
+- mục tiêu là chỉ 1 trong 2 có giá trị
+- nên phải add constraint để tránh insert sai data
+- 1 cái chưa hay ở đây là, nếu muốn like nhiều cái khác, thì phải tạo nhiều cột khác
+
+cách 3: cách dễ nhất
+- tạo 2 table, mỗi table đại diện cho 1 kiểu like: post_likes, comment_likes table
+- cái cùi bắp của nó thì cũng như ở trên, nếu like nhiều thứ, thì tạo nhiều table
+- nếu muốn thống kê số lượng likes thì phải dùng union hay view
+- cái tốt ở phương pháp này là nếu có kiểu reaction type, thì thêm cột reaction_type vào
+ table post_likes
+
+ ! Ở đây ta sẽ chọn cách 2 với lý do sau:
+ - khi like, thì chỉ like 1 post hay 1 comment
+ - ko có reaction type, kiểu như love, haha, sad...
+ - ko có sự khác nhau khi like 1 post hay 1 comment
+ - nên ko cần tạo 2 bảng khác nhau, vì khi tạo 2 bảng, ta có thể thêm những thông tin khác
+ cụ thể thêm cho từng bảng
+ - 1 là like, 2 là xóa like, ko có khái niệm update like, nên là trong bảng ko cần updated_at
+ - còn created_at ở đây có thể dùng để thống kê, những like nào gần đây
+*/
